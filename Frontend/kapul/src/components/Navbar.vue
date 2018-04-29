b928b7681686a72df8ea9a698a33cde867f0b4f9
Nicolas Garnier <nicolas.garnier@nikositech.com>
2018-04-29 18:36:22 +0200

    [FRONT] Add new Components

---
+++ Frontend/kapul/src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-if="connected">
+      <p>Connected</p>
+    </div>
+    <div v-else>
+      <p>Not connected</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default
+  {
+    props: ["connected"]
+  }
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ Frontend/kapul/src/components/Users.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="users">
+  </div>
+</template>
+
+<script>
+
+  export default
+  {
+    components:
+    {
+    },
+
+    data: function()
+    {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
