a2bc1ff930cc37f3c297f39c5de27706bc6d23f4
Kelmatou <antoine.clop@epita.fr>
2018-05-08 16:46:15 +0200

    Adding RabbitMq Extension methods

---
+++ Backend/src/Kapul.Common/RabbitMq/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using Kapul.Common.Commands;
+using Kapul.Common.Events;
+using RawRabbit;
+using RawRabbit.Pipe;
+
+namespace Kapul.Common.RabbitMq
+{
+    public static class Extensions
+    {
+        public static Task WithCommandHandlerAsync<TCommand>(this IBusClient bus,
+            ICommandHandler<TCommand> handler) where TCommand : ICommand
+        => bus.SubscribeAsync<TCommand>(message => handler.HandleAsync(message),
+            context => context.UseConsumerConfiguration(
+                configuration => configuration.FromDeclaredQueue(
+                    queue => queue.WithName(GetQueueName<TCommand>()))));
+
+        public static Task WithEventHandlerAsync<TEvent>(this IBusClient bus,
+            IEventHandler<TEvent> handler) where TEvent : IEvent
+        => bus.SubscribeAsync<TEvent>(message => handler.HandleAsync(message),
+            context => context.UseConsumerConfiguration(
+                configuration => configuration.FromDeclaredQueue(
+                    queue => queue.WithName(GetQueueName<TEvent>()))));
+
+        private static string GetQueueName<T>()
+        {
+            return $"{Assembly.GetEntryAssembly().GetName()}/{typeof(T).Name}";
+        }
+    }
+}
