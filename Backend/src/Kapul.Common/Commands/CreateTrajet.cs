c9ee1d0aa9e208367512bbaa043f19fa3f2d68d5
Kelmatou <antoine.clop@epita.fr>
2018-05-08 12:34:11 +0200

    [API GATEWAY] adding command creation implementation

---
+++ Backend/src/Kapul.Common/Commands/CreateTrajet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kapul.Common.Commands
+{
+    public class CreateTrajet : IAuthenticatedCommand
+    {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public string Departure { get; set; }
+
+        public DateTime DepartureTime { get; set; }
+
+        public string Arrival { get; set; }
+
+        public double Price { get; set; }
+
+        public long SitsAvailable { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
