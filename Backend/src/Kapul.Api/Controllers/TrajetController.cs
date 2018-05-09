2fa89b0c8abdcc4348f192a2b2a7346eca6423e6
SeiyaR <redon_c@epita.fr>
2018-05-09 13:10:11 +0200

    [API GATEWAY] adding first controllers defining endpoint

---
+++ Backend/src/Kapul.Api/Controllers/TrajetController.cs
@@ -0,0 +1,30 @@
+﻿using Kapul.Common.Commands;
+using Microsoft.AspNetCore.Mvc;
+using RawRabbit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kapul.Api.Controllers
+{
+    [Route("[controller]")]
+    public class TrajetsController: Controller
+    {
+        private readonly IBusClient _busClient;
+
+        public TrajetsController(IBusClient busClient)
+        {
+            this._busClient = busClient;
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> Post([FromBody]CreateTrajet command)
+        {
+            command.Id = Guid.NewGuid();
+            command.CreatedAt = DateTime.UtcNow;
+            await _busClient.PublishAsync(command);
+            return Accepted($"trajet/{command.Id}");
+        }
+    }
+}
