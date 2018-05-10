92e77916dbdeba76d05def54003611c11cea2765
SeiyaR <redon_c@epita.fr>
2018-05-10 19:27:51 +0200

    [API GATEWAY] adding jwt class to store token

---
+++ Backend/src/Kapul.Common/Auth/IJwtHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kapul.Common.Auth
+{
+    public interface IJwtHandler
+    {
+        JsonWebToken Create(Guid userId);
+    }
+}
