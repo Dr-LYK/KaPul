863e002f14b3654301b61560ee69e282333003ba
SeiyaR <redon_c@epita.fr>
2018-05-12 18:09:07 +0200

    [TESTS] adding test class for Trajets

---
+++ Backend/tests/Kapul.Services.Trajet.Tests/Unit/Services/TrajetsServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kapul.Services.Trajet.Tests.Unit.Services
+{
+    public class TrajetsServiceTests
+    {
+        [Fact]
+        public async Task trajets_service_add_async_should_succeed()
+        {
+            // Need to implement trajet service first
+        }
+    }
+}
