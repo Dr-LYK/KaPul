25ea2f1d2a259df169e234bd0f4e0331438e6c9e
Farah Nedjadi <farah.nedjadi@epita.fr>
2018-05-18 23:23:27 +0200

    [API GATEWAY] adding car route

---
+++ Backend/src/Kapul.Common/Commands/GetCar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kapul.Common.Commands
+{
+    class GetCar : IAuthenticatedCommand
+    {
+        public Guid Id { get; }
+        public Guid UserId { get; set; }
+    }
+}
