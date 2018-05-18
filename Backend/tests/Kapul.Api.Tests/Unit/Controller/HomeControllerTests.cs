b5b635dc2c56d2918eb58fe7d1d74335f7a61671
Farah Nedjadi <farah.nedjadi@epita.fr>
2018-05-18 23:50:56 +0200

    [TESTS] Edit bug

--- Backend/tests/Kapul.Api.Tests/Unit/Controller/HomeControllerTests.cs
+++ Backend/tests/Kapul.Api.Tests/Unit/Controller/HomeControllerTests.cs
@@ -1,6 +1,9 @@
 ﻿using FluentAssertions;
 using Kapul.Api.Controllers;
+using Kapul.Api.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RawRabbit;
 using Xunit;
 
 namespace Kapul.Api.Tests.Unit.Controller
@@ -10,11 +13,13 @@ namespace Kapul.Api.Tests.Unit.Controller
         [Fact]
         public void Home_controller_get_should_return_string_content()
         {
-            HomeController controller = new HomeController();
+            var clientMock = new Mock<IBusClient>();
+            var trajetRespositoryMock = new Mock<ITrajetRepository>();
+            HomeController controller = new HomeController(clientMock.Object, trajetRespositoryMock.Object);
             IActionResult result = controller.Get();
             ContentResult contentResult = result as ContentResult;
             contentResult.Should().NotBeNull();
-            contentResult.Content.ShouldAllBeEquivalentTo("Kapul API");
+            contentResult.Content.Should().BeEquivalentTo("Kapul API");
         }
     }
 }
